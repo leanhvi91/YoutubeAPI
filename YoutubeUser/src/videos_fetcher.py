02effcf16b4463f07b2dc9648a4ebf34d4d4bb09
villa <vi@mail.com>
2018-01-25 20:37:19 +0700

    add channel detail

--- YoutubeUser/src/videos_fetcher.py
+++ YoutubeUser/src/videos_fetcher.py
@@ -70,22 +70,90 @@ def save_to_json_lines(items, filePath):
             f.write(line)
 
 
-if __name__=="__main__":
+def get_channel_info(ids=[]):
+    """
 
-    items = []
-
-    channelId = "UCzR-rom72PHN9Zg7RML9EbA"
-
-    key = "AIzaSyCVB2uxo0LC52PuOdb_yPKH_bMz1d3mPJU"
-
-    querystring = {
-        "channelId": channelId,
-        "part": "snippet",
-        "key": key,
-        "type": "video",
-        "maxResults": 50
+    :return:
+    """
+    if not ids:
+        return {}
+    _ids = ",".join(ids)
+    url = "https://www.googleapis.com/youtube/v3/channels"
+    headers = {
+        'cache-control': "no-cache",
+        'postman-token': "a7befa87-43cd-82f5-4ad2-ddab7f986f16"
     }
 
-    filePath = ("%s/%s/videos.txt" % (data_home, channelId))
+    querystring = {
+        "id": _ids,
+        "part": "snippet",
+        "key": "AIzaSyCVB2uxo0LC52PuOdb_yPKH_bMz1d3mPJU"
+    }
 
-    get_channel_videos(querystring, filePath)
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    if not response.ok:
+        return
+    responseObject = response.json()
+    totalItems = responseObject["pageInfo"]["totalResults"]
+    items = responseObject["items"]
+    return items
+
+
+def get_video_info(ids=[]):
+    """
+
+    :param ids:
+    :return:
+    """
+    if not ids:
+        return {}
+    _ids = ",".join(ids)
+    url = "https://www.googleapis.com/youtube/v3/videos"
+    headers = {
+        'cache-control': "no-cache",
+        'postman-token': "a7befa87-43cd-82f5-4ad2-ddab7f986f16"
+    }
+
+    querystring = {
+        "id": _ids,
+        "part": "snippet",
+        "key": "AIzaSyCVB2uxo0LC52PuOdb_yPKH_bMz1d3mPJU"
+    }
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    if not response.ok:
+        return
+    responseObject = response.json()
+    totalItems = responseObject["pageInfo"]["totalResults"]
+    items = responseObject["items"]
+    return items
+
+if __name__=="__main__":
+
+    # items = []
+    #
+    # channelId = "UCzR-rom72PHN9Zg7RML9EbA"
+    #
+    # key = "AIzaSyCVB2uxo0LC52PuOdb_yPKH_bMz1d3mPJU"
+    #
+    # querystring = {
+    #     "channelId": channelId,
+    #     "part": "snippet",
+    #     "key": key,
+    #     "type": "video",
+    #     "maxResults": 50
+    # }
+    #
+    # filePath = ("%s/%s/videos.txt" % (data_home, channelId))
+    #
+    # get_channel_videos(querystring, filePath)
+
+    ids = ["UC6E2mP01ZLH_kbAyeazCNdg", "UC08wrceRCWhajGG4udhETww", "UCd_vju15XYyTXgL-S3_2HtA",
+           "UCd_vju15XYyTXgL-S3_2HtA", "UCJsSEDFFnMFvW9JWU6XUn0Q", "UCkEBDbzLyH-LbB2FgMoSMaQ",
+           "UCvJiYiBUbw4tmpRSZT2r1Hw", "UCwmZiChSryoWQCZMIQezgTg", "UCzR-rom72PHN9Zg7RML9EbA", "UCzWQYUVCpZqtN93H8RR44Qw"]
+
+    items = get_channel_info(ids=ids)
+    filePath = "/home/vila/projects/YoutubeAPI/YoutubeUser/data/channels.txt"
+    save_to_json_lines(items=items, filePath=filePath)
+
+    print(items)
